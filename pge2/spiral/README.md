4a9a85a05cf376213c5595cee4d209929ba330d6
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2025-06-08 21:36:24 -0400

    On branch develop 	new file:   README.md 	new file:   main.m 	new file:   vds.m 	new file:   writeIntSpiralFW.m

---
+++ pge2/spiral/README.md
@@ -0,0 +1,4 @@
+# 2D interleaved spiral sequence for Pulseq on GE 
+
+Test new rotation extension in Pulseq
+
---
+++ pge2/spiral/main.m
@@ -0,0 +1,29 @@
+% actions
+createSequenceFile = 1;
+reconstruct = 0;
+
+if createSequenceFile
+    % write spiral.seq
+    system('git clone --branch dev git@github.com:pulseq/pulseq.git');
+    addpath pulseq/matlab
+    system('git clone --branch v1.9.1 git@github.com:toppeMRI/toppe.git');
+    addpath toppe
+    writeIntSpiralFW;   
+
+    % convert to spiral.pge
+%    system('git clone --branch v2.2.3-beta git@github.com:HarmonizedMRI/PulCeq.git');
+%    addpath PulCeq/matlab
+    addpath ~/github/jfnielsen/PulCeq/matlab  % Jon's fork
+    ceq = seq2ceq('spiral.seq');
+    pislquant = 1;   % number of ADC events used for receive gain calibration
+    writeceq(ceq, 'spiral.pge', 'pislquant', pislquant);
+end
+
+if reconstruct
+    addpath ~/Programs/orchestra-sdk-2.1-1.matlab/
+    PinvRecon_IntSpiral;
+
+    % or:
+    % recon_nufft;
+end
+
