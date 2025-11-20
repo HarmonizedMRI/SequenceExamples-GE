2345ec55f30e61add5d9fefd2b3f3d25db883edd
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2025-11-20 12:25:42 -0500

    On branch main 	modified:   setup.m

--- pge2/2DGRE/setup.m
+++ pge2/2DGRE/setup.m
@@ -1,17 +1,13 @@
 % get Pulseq toolbox
 system('git clone --branch v1.5.1 git@github.com:pulseq/pulseq.git');
 addpath pulseq/matlab
-warning('OFF', 'mr:restoreShape');  % turn off Pulseq warning for spirals
 
 % get toolbox to convert .seq file to a PulCeq (Ceq) object
 system('git clone --branch tv7 git@github.com:HarmonizedMRI/PulCeq.git');
 addpath PulCeq/matlab
 
-curdir = pwd; cd ~/github/mirt; setup; cd(curdir);
-
-% To load the ScanArchive raw data files you will need 
-% the Orchestra toolbox which is available for download 
-% at http://weconnect.gehealthcare.com/
+% To load the ScanArchive raw data files you will need the Orchestra toolbox
+% which is available for download at http://weconnect.gehealthcare.com/ 
 addpath ~/Programs/orchestra-sdk-2.1-1.matlab/
 
 % get 'im' display function (optional)
